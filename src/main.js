852f9fe315ea7e8d2deb958cb8e4fd7dfae79ff9
gpf131012 <1654648712@qq.com>
2019-11-12 21:22:35 +0800

    黑马头条pc-准备资源-整理目录

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import elementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import './style/index.less'
 Vue.config.productionTip = false
 Vue.use(elementUI)
 new Vue({
