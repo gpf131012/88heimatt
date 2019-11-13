c54f6ae25fce7c91ae637e6abe09e5c0cd29b4ac
gpf131012 <1654648712@qq.com>
2019-11-13 17:12:19 +0800

    黑马头条PC-登录模块-页面布局及样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>登录</div>
+  <div class="login">
+      <el-card class="login-card">
+          <div class="titel">
+              <img src="../../assets/img/logo_index.png" alt="">
+          </div>
+    <el-form style="margin-top:20px">
+        <el-form-item>
+            <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-input style="width:230px" placeholder="请输入验证码"></el-input>
+            <el-button style="float:right" plain>获取验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+             <el-checkbox>我同意<a href="" style="color:blue">用户协议</a>与<a href="" style="color:blue">霸王条款</a></el-checkbox>
+        </el-form-item>
+        <el-form-item>
+       <el-button style="width:100%" type="primary">登录</el-button>
+        </el-form-item>
+    </el-form>
+
+      </el-card>
+</div>
 </template>
 
 <script>
@@ -8,6 +30,24 @@ export default {
 }
 </script>
 
-<style>
+<style lang='less' scoped>
+.login{
+    background-image: url(../../assets/img/login_bg.jpg);
+    background-size: cover;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items:center;
+    .login-card{
+        width: 400px;
+        height: 360px;
+        .titel{
+            text-align: center;
+            img{
+            height: 40px;
+            }
+        }
+    }
+}
 
 </style>
