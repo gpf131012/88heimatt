44d3b66ad2f6c05b054897b16f18bc5045e8c910
gpf131012 <1654648712@qq.com>
2019-11-16 18:20:25 +0800

    展示所有文章-数据分页-Loading 效果

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -29,18 +29,20 @@
       range-separator="-"
       start-placeholder="开始日期"
       end-placeholder="结束日期">
+
     </el-date-picker>
   </el-form-item>
   </el-form>
 </el-card>
 <el-card>
 <div slot="header" class="clearfix">
-<span>共找到59806条符合条件的内容</span>
+<span>共找到{{totalCount}}条符合条件的内容</span>
 </div>
 
 <el-table
       :data="articlesL"
-      style="width: 100%">
+      style="width: 100%"
+       v-loading="loading">
       <el-table-column
         prop="date"
         label="封面"
@@ -57,7 +59,7 @@
       <el-table-column
         prop="status"
         label="状态">
-        <template slot-scope="scope">
+        <template slot-scope="scope"><!-- 声明slot-scope=sxope  -->
           <!-- <span>{{l}}</span> -->
           <el-tag :type="articleStatusL[scope.row.status].type">
             {{articleStatusL[scope.row.status].label}}</el-tag>
@@ -72,14 +74,23 @@
         prop="operate"
         label="操作">
         <template>
-        <i class="el-icon-edit"></i>
-
-        <i class="el-icon-delete"></i>
+        <!-- <i class="el-icon-edit" style="margin-right:8 px">编辑</i> -->
+         <el-link icon="el-icon-edit">编辑</el-link>
+        <!-- <i class="el-icon-delete">删除</i> -->
+         <el-link icon="el-icon-delet"  class="el-icon-delete">删除</el-link>
         </template>
       </el-table-column>
     </el-table>
 </el-card>
-
+<!-- 分页 -->
+<!-- 默认按照10条每页划分 -->
+<el-pagination
+  background
+  layout="prev, pager, next"
+  :total="totalCount"
+  :disabled="loading"
+  @current-change="onPageChange">
+</el-pagination>
   </div>
 </template>
 
@@ -118,14 +129,18 @@ export default {
           type: 'danger',
           label: '已删除'
         }
-      ]
+      ],
+      totalCount: 0,
+      loading: true
     }
   },
   created () {
     this.loadArticles()
   },
   methods: {
-    loadArticles () {
+    // page用于传递
+    loadArticles (page = 1) {
+      this.loading = true
       const token = window.localStorage.getItem('user-token')
       this.$axios({
         method: 'GET',
@@ -135,12 +150,24 @@ export default {
         // 后端把token放到请求头中，使用的名字Authorization
         // token要求格式Bearer 用户token
           Authorization: `Bearer ${token}`
+        },
+        params: {
+          page,
+          per_page: 10
         }
       }).then(res => {
         this.articlesL = res.data.data.results
+        // 更新总记录数
+        this.totalCount = res.data.data.total_count
       }).catch(err => {
         console.log(err)
+      }).finally(() => {
+        this.loading = false
       })
+    },
+    onPageChange (page) {
+      // console.log(page)
+      this.loadArticles(page)
     }
   }
 }
